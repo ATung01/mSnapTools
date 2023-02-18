776cac42df67e79ad858ad3995aff88960645f61
ATung01 <acc.tung@gmail.com>
2023-02-17 23:55:05 -0500

    different version of tess function

--- agathaClick.js
+++ agathaClick.js
@@ -1,9 +1,32 @@
-import Tesseract from 'tesseract.js';
+import { createWorker } from 'tesseract.js'
 
-Tesseract.recognize(
-  'images/textTest.png',
-  'eng',
-  { logger: m => console.log(m) }
-).then(({ data: { text } }) => {
-  console.log(text);
-})
+
+
+// function preprocessImage(canvas) {
+//     const level = 0.4
+//     const radius = 1
+//     const ctx = canvas.getContext('2d')
+//     const image = ctx.getImageData(0,0,canvas.width, canvas.height)
+//     blurARGB(image.data, canvas, radius)
+//     dilate(image.data, canvas)
+//     invertColors(image.data)
+//     thresholdFilter(image.data, level)
+//     return image
+//     }
+
+const worker = await createWorker({
+    logger: m => console.log(m)
+});
+
+// let processedImg = preprocessImage('images/textTest.png')
+
+(async () => {
+    await worker.load()
+    await worker.loadLanguage('eng')
+    await worker.initialize('eng')
+    const { data: { text } } = 
+        await worker.recognize('images/textTest.png')
+    console.log(text)
+    await worker.terminate()
+})()
+
